19fa2dc2c1a005b64b8636d1ee3f8c8af08f8d5f
aimansharif <33303239+aimansharif@users.noreply.github.com>
2018-06-11 12:17:44 +0600

    Updated two-sum-list

--- Two-Sum-List.py
+++ Two-Sum-List.py
@@ -11,18 +11,12 @@ def twoSum(nums, target):
     >> [0, 2]
     """
     
+    dct = {}
     for i in range(len(nums)):
-        for j in range(len(nums)):
-            if i != j:
-                if nums[i] + nums[j] == target:
-                    return [i, j]
+        key = target - nums[i]
+        if key in dct:
+            return [dct[key],i]
+        else:
+            dct[nums[i]] = i
                 
-nums = [2, 7, 12]
-nums1 = [2, 9, 7]
-nums2 = [1, 32, 5, 10, 15, 20]
-nums3 = [-5, -20, 25, 40, 100, 250, 2]
 
-print(twoSum(nums, 9))
-print(twoSum(nums1, 9))
-print(twoSum(nums2, 52))
-print(twoSum(nums3, 35))
