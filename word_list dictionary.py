9c5f8012356a9113ace84238671ab2efd1a10add
aimansharif <33303239+aimansharif@users.noreply.github.com>
2018-08-06 13:10:11 +0600

    word_list dictionary updated

--- word_list dictionary.py
+++ word_list dictionary.py
@@ -5,6 +5,7 @@ def build_dictionary(words):
   Given a list of words, returns a dictionary with the length of each word as a key and the word as a value.
   Counts the number of letter in each word. 
   >>> word_list = ['the', 'the', 'the', 'hello', 'hi', 'hi', 'hi', 'in', 'world', 'world']
+  >>> {5: ['world', 'hello'], 2: ['hi', 'in'], 3: ['the']}
   """
   
   word_set = set(words)
